b89693c8ecb3857ae25fb82b62f8ef7ae07ff66c
Benjamin Rosman <rosman.benji@gmail.com>
2017-01-24 18:01:36 +0100

    Begun to implement the DefualtGameView

--- src/com/jensen/game/view/DefualtGameView.java
+++ src/com/jensen/game/view/DefualtGameView.java
@@ -2,12 +2,79 @@ package com.jensen.game.view;
 
 import com.jensen.game.inteface.GameView;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.Box;
+import javax.swing.Icon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseListener;
 
 public class DefualtGameView extends JPanel implements GameView {
+
+    private GridPanel gridPanel;
+    private JButton menuButton;
+    private JLabel titleLabel;
+    private JLabel messageLabel;
+
+    public DefualtGameView() {
+        super();
+        initPanel("A Grid Game");
+    }
+
+    public DefualtGameView(String name) {
+        super();
+        initPanel(name);
+    }
+
+    private void initPanel(String name) {
+        gridPanel = new GridPanel();
+
+        setLayout(new BorderLayout());
+        add(initHeadPanel(name), BorderLayout.PAGE_START);
+        add(gridPanel, BorderLayout.CENTER);
+        add(initMessagePanel(), BorderLayout.PAGE_END);
+    }
+
+    //alternativ till att HeadPanel ärver ifrån JPanel
+    private JPanel initHeadPanel(String name) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        titleLabel = new JLabel(name);
+        titleLabel.setHorizontalAlignment(JLabel.CENTER);
+        panel.add(titleLabel, BorderLayout.CENTER);
+
+        menuButton = new JButton("Menu");
+        menuButton.setActionCommand("Menu");
+        panel.add(menuButton, BorderLayout.LINE_END);
+
+        panel.add(Box.createHorizontalStrut(30), BorderLayout.LINE_START);
+        return panel;
+    }
+
+    private JPanel initMessagePanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        panel.add(Box.createHorizontalStrut(20));
+
+        messageLabel = new JLabel();
+        messageLabel.setHorizontalAlignment(JLabel.CENTER);
+        panel.add(messageLabel);
+        return panel;
+    }
+
+    public void setTitleText(String text) {
+        titleLabel.setText(text);
+    }
+
+    public void setTitleIcon(Icon icon) {
+        titleLabel.setIcon(icon);
+    }
 
     @Override
     public void updateCell(int x, int y, String status) {
@@ -16,7 +83,7 @@ public class DefualtGameView extends JPanel implements GameView {
 
     @Override
     public void updateMessage(String message) {
-
+        messageLabel.setText(message);
     }
 
     @Override
@@ -26,7 +93,7 @@ public class DefualtGameView extends JPanel implements GameView {
 
     @Override
     public void addMenuButtonListener(ActionListener l) {
-
+        menuButton.addActionListener(l);
     }
 
     @Override
@@ -36,6 +103,6 @@ public class DefualtGameView extends JPanel implements GameView {
 
     @Override
     public Component getComponent() {
-        return null;
+        return this;
     }
 }
