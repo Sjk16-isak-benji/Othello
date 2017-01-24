a4c6c9b7a8e30e366d6c5ea9418831ab83759bee
Benjamin Rosman <rosman.benji@gmail.com>
2017-01-24 17:56:12 +0100

    added constructor

--- src/com/jensen/game/othello/view/OthelloGameView.java
+++ src/com/jensen/game/othello/view/OthelloGameView.java
@@ -4,6 +4,10 @@ import com.jensen.game.view.DefualtGameView;
 
 public class OthelloGameView extends DefualtGameView {
 
+    public OthelloGameView() {
+        super("Othello");
+    }
+
     @Override
     public void updateCell(int x, int y, String status){
 
