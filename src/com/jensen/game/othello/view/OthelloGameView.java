da5991d52436791474bbada4d7b65fa815794579
Benjamin Rosman <rosman.benji@gmail.com>
2017-01-25 18:26:54 +0100

    added javadoc for OthelloGameView

--- src/com/jensen/game/othello/view/OthelloGameView.java
+++ src/com/jensen/game/othello/view/OthelloGameView.java
@@ -1,15 +1,25 @@
 package com.jensen.game.othello.view;
 
 import com.jensen.game.view.DefualtGameView;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+/**
+ * An Extension of the defualt game view to display a othello game.
+ * Overrides updateCell to display othello pieces instead of text.
+ *
+ * @see DefualtGameView
+ */
 public class OthelloGameView extends DefualtGameView {
 
+    /**
+     * Creates a othello game view with Othello written in the header.
+     */
     public OthelloGameView() {
         super("Othello");
     }
 
     @Override
-    public void updateCell(int x, int y, String status){
-
+    public void updateCell(int x, int y, String status) {
+        throw new NotImplementedException();
     }
 }
