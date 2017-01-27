73ad52813999c3e3a2e72ceef3961c7d1638f295
Benjamin Rosman <rosman.benji@gmail.com>
2017-01-27 10:36:04 +0100

    main

--- src/com/jensen/game/Main.java
+++ src/com/jensen/game/Main.java
@@ -7,7 +7,7 @@ import com.jensen.game.view.Window;
 
 import javax.swing.JFrame;
 
-public class Game {
+public class Main {
 
     public static void main(String[] args) {
         GameView game = new OthelloGameView();
