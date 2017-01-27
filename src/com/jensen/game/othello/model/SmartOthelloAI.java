da582f7eb4b686efcdbf8e7acb7654c900468feb
Isak Wertwein <iswer1@hotmail.com>
2017-01-27 11:28:08 +0100

    improved AI + smart AI

--- src/com/jensen/game/othello/model/SmartOthelloAI.java
+++ src/com/jensen/game/othello/model/SmartOthelloAI.java
@@ -3,24 +3,24 @@ package com.jensen.game.othello.model;
 import com.jensen.game.inteface.AI;
 import com.jensen.game.model.Cell;
 
-class DumbOthelloAI implements AI {
+class SmartOthelloAI implements AI {
 
     private OthelloPlayer player;
 
-    public DumbOthelloAI(OthelloPlayer player) {
+    public SmartOthelloAI(OthelloPlayer player) {
         this.player = player;
     }
 
     @Override
     public Cell getMove(Cell[] cells) {
-        int[] weights = new OthelloCellWeightCalculator(2, 1, 1).getWeights(cells, player);
+        int[] weights = new OthelloCellWeightCalculator(10, 5, 1).getWeights(cells, player);
         int maxIndex = 0;
         int maxWeight = 0;
 
         for (int i = 0; i < weights.length; i++) {
             int weight = weights[i];
 
-            if (weight > maxWeight && Math.random() > 0.5) {
+            if (weight > maxWeight) {
                 maxIndex = i;
                 maxWeight = weight;
             }
