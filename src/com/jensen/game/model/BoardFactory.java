d6584122d32f0da939f241f7c84253e1c246ce5a
Isak Wertwein <iswer1@hotmail.com>
2017-01-27 13:40:55 +0100

    Separate OthelloBoardFactory with additional methods

--- src/com/jensen/game/model/BoardFactory.java
+++ src/com/jensen/game/model/BoardFactory.java
@@ -3,15 +3,15 @@ package com.jensen.game.model;
 /**
  * A factory class for creation of game boards.
  */
-public abstract class BoardFactory {
+public class BoardFactory {
     /**
-     * Creates a normal board of a specific size.
+     * Creates an empty board of a specific size.
      *
      * @param width  The width of the board.
      * @param height The height of the board.
      * @return A new board.
      */
-    public static Board createBoard(int width, int height) {
+    public Board createBoard(int width, int height) {
         return new Board(width, height);
     }
 }
