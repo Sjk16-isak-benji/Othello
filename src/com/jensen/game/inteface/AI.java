fc0e9f1e36e31f0dd653a229fa6103a68bcb011a
Benjamin Rosman <rosman.benji@gmail.com>
2017-01-31 20:47:06 +0100

    added missing javadoc for AI.java

--- src/com/jensen/game/inteface/AI.java
+++ src/com/jensen/game/inteface/AI.java
@@ -6,5 +6,13 @@ import com.jensen.game.model.Cell;
  * An inteface for a players AI.
  */
 public interface AI {
+
+    /**
+     * Given all the available cells on which this AI can make a move on it returns
+     * the cell on which to place a piece.
+     *
+     * @param cells The cells on which this AI can make a move on.
+     * @return The cell it's programmed to return.
+     */
     Cell getMove(Cell[] cells);
 }
